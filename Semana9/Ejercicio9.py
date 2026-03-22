383ca902664c9272a59960b79e83187761aa622c
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:22:35 -0600

    Semana 9 ejercicio 9 Solicita una frase y muestra si empieza por 'Hola'

---
+++ Semana9/Ejercicio9.py
@@ -0,0 +1,10 @@
+# Ejercicio 9
+
+# Solicitar frace
+frase = input("Ingrese una frase: ")
+
+# Verificar
+if frase.startswith("Hola"):
+    print("La frase empieza con 'Hola' ")
+else:
+    print("La frase NO empieza con 'Hola' ")
