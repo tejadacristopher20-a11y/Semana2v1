26fa6d5593405d4f332b209df5ccb029ed1089f9
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:11:05 -0600

    Semana 9 ejercicio 5 solicita una frase y muestra primera y ultima letra

---
+++ Semana9/Ejercicio5.py
@@ -0,0 +1,12 @@
+# Ejercicio 5
+
+# ingrese frase
+frase = input("Ingrese una frase: ")
+
+# primera y ultima ltra
+primera = frase[0]
+ultima = frase[-1]
+
+# mostrar
+print("Primera letra:", primera)
+print("Ultima letra:", ultima)
