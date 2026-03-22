3060a88f095fac151253afe45c512da5d015a173
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:23:11 -0600

    Semana 9 ejercicio 10 Solicita una frase y verifica si es que termina en .

---
+++ Semana9/Ejercicio10.py
@@ -0,0 +1,10 @@
+# Ejercicio 10
+
+# ingrese frace
+frase = input("Ingrese una frase: ")
+
+# validar
+if frase.endswith("."):
+    print("La frase termina con un punto ")
+else:
+    print("La frase NO termina con un punto ")
