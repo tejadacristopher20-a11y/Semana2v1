b76398d10521112ef9380301a4e11d79f6f7bc24
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:10:20 -0600

    Semana 9 ejercicio 4 solicita un correo y valida si tiene @

---
+++ Semana9/Ejercicio4.py
@@ -0,0 +1,10 @@
+# Ejercicio 4
+
+# Ingrese correo
+correo = input("Ingrese su correo electrónico: ")
+
+# Verifica @
+if "@" in correo:
+    print("El correo parece válido")
+else:
+    print("El correo no es válido")
