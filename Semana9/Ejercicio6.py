5eb41765995bc0bfe4f97415e13f3e48bb72fc6c
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:17:06 -0600

    Semana 9 ejercicio 6 Solicita un nombre y lo separa en palabra por linea

---
+++ Semana9/Ejercicio6.py
@@ -0,0 +1,11 @@
+# Ejercicio 6
+
+# ingrese nombre completo
+nombre = input("Ingrese su nombre completo: ")
+
+# separar palabras
+palabra = nombre.split()
+
+# mostrar
+for palabra in palabra:
+    print(palabra)
