f200467fbcfa0b84d62a3bc743f1ef8b37961174
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:21:36 -0600

    Semana 9 ejercicio 8 solicita una frase y cuenta cuantas veces aparece la a

---
+++ Semana9/Ejercicio8.py
@@ -0,0 +1,10 @@
+# Ejercicio 8
+
+# ingrese frase
+frase = input("Ingrese una frase: ")
+
+# cuantas veces aparece a
+cantidad = frase.count("a")
+
+# print
+print("La letra 'a' aparece:", cantidad, "veces")
