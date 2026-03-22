7c624c29adef5495ae12066a20766275e870612d
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:08:03 -0600

    Semana 9 ejercicio 2 programa que solicita frase

---
+++ Semana9/Ejercicio2.py
@@ -0,0 +1,13 @@
+# Ejercicio 2
+
+# Solicite la frase
+frase = input("Ingrese una frase: ")
+
+# mostrar la frase
+print("Frase:", frase)
+
+# frase en mayusculas
+print("En mayúsculas:", frase.upper())
+
+# frase en minusculas
+print("En minúsculas:", frase.lower())
