e01a0c941e62e73b1fd65eb9a7e679347ca918a3
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:20:34 -0600

    Semana 9 ejercicio 7 solicita una frase y donde dice 'Python' remplaza por Programación

---
+++ Semana9/Ejercicio7.py
@@ -0,0 +1,10 @@
+# Ejercicio 7
+
+# solicitar frase
+frase = input("Ingrese una frase:")
+
+# Reeemplazar
+nuevaFrase = frase.replace("Python", "Programación")
+
+# mostrar
+print("Frase modificada:", nuevaFrase)
