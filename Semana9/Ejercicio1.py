d6251cccf8bea7c901e7281251da2814f1f4acda
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-03-21 19:06:59 -0600

    Semana 9 ejercicio 1 nombre de usuario completo

---
+++ Semana9/Ejercicio1.py
@@ -0,0 +1,13 @@
+# Ejercicio 1
+
+# solicitar nombre completo
+nombre = input("Ingrese su nombre completo: ")
+
+# Nombre en mayusculas
+print("En mayúsculas:", nombre.upper())
+
+# Nombre en minusculas
+print("En minúsculas:", nombre.lower())
+
+# Cantidad de caracteres
+print("Cantidad de caracteres:", len(nombre))
