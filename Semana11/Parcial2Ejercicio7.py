7fedc87443eb5ce0df54f3742a22651470416ab1
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-04-10 19:56:07 -0600

    Parcial 2, Ejercicio 7 toma 42 rellena con 0 a la izquierda hasta llegar a 5 caracteres y verifica si termina en 2

---
+++ Semana11/Parcial2Ejercicio7.py
@@ -0,0 +1,8 @@
+numero = "42"
+
+numeroMixto = numero.zfill(5)
+
+numeroBoleano = numeroMixto.endswith("2")
+
+print("El nuevo numero es: ", numeroMixto)
+print("Este nuevo numero termina en 2: ", numeroBoleano)
