d478ad6d356d664a4ee0df42378bc6c5dd376307
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-04-10 19:45:10 -0600

    Parcial 2, Ejercicio 1  elimina espacios de la palabra y cuenta cuantas veces aparece la letra e

---
+++ Semana11/Parcial2Ejercicio1.py
@@ -0,0 +1,8 @@
+cadena = "elefante "
+
+cadenaLimpia = cadena.strip()
+
+aparicionE = cadenaLimpia.count("e")
+
+print("Cadena sin espacios: ", cadenaLimpia)
+print("Cantidad de letras 'e': ", aparicionE)
