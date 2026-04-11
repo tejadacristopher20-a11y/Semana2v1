59ea67e870cfb4703147b94ce87de5c4fd316f1f
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-04-10 19:48:16 -0600

    Parcial 2, Ejercicio 3 remueve el ING. de mi nombre y combierte el texto a mayusculas

---
+++ Semana11/Parcial2Ejercicio3.py
@@ -0,0 +1,8 @@
+nombreC = "ING. Cristopher David"
+
+sinFuturo = nombreC.replace("ING. ", "")
+
+nombreMayuscula = sinFuturo.upper()
+
+print("Nombre sin ING: ", sinFuturo)
+print("Nombre en mayusculas: ", nombreMayuscula)
