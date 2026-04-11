8fcbb9fc9efc504c4a117e5bae732e3ecd89b931
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-04-10 19:47:01 -0600

    Parcial 2, Ejercicio 2 muestra mi nombre en mayusculas la primera letra y luego remplaza el nombre por el apellido

---
+++ Semana11/Parcial2Ejercicio2.py
@@ -0,0 +1,8 @@
+nombre = "cristopher david"
+
+nombreMayusculas = nombre.title()
+
+apellido = nombreMayusculas.replace("Cristopher David", "Salmeron Tejada")
+
+print("Nombre con mayusculas: ", nombreMayusculas)
+print("Apellidos: ", apellido)
