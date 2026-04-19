174b20e8617add235f22bdaece7ffa97bd9d8fb5
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-04-18 21:27:24 -0600

    Semana 12 Ejercicio 1 Pide un número al usuario e indica si es positivo, negativo o cero usando

---
+++ Semana12/Semana12Ejercicio1.py
@@ -0,0 +1,11 @@
+# Ejercicio 1:
+# Pide un número al usuario e indica si es positivo, negativo o cero usando if, elif y else.
+
+numeroX = float(input("Ingresa un numero: "))
+
+if numeroX > 0:
+    print("Tu numero es positivo")
+elif numeroX < 0:
+    print("Tu numero es negativo")
+else:
+    print("Tu numero es 0")
