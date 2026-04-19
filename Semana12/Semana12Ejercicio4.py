6554380a04e63cc3747cce7d1190af3d53598069
tejadacristopher20-a11y <tejadacristopher20@gmail.com>
2026-04-18 21:29:02 -0600

    Semana 12 Ejercicio 4 Pide un número y determina si es par o impar.

---
+++ Semana12/Semana12Ejercicio4.py
@@ -0,0 +1,9 @@
+# Ejercicio 4:
+# Pide un número y determina si es par o impar.
+
+numeroX = int(input("ingresa un numero para saber si es par o impar: "))
+
+if numeroX % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
